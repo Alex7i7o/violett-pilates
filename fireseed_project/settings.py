881329fa33239c5d85c634e8fc6b0979af62e8b2
Alex7i7o <alexxaguero@gmail.com>
2026-08-27 19:12:54 -0300

    chore: migrate from sqlite to postgresql with docker compose

--- fireseed_project/settings.py
+++ fireseed_project/settings.py
@@ -14,6 +14,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,13 +97,10 @@ WSGI_APPLICATION = 'fireseed_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 20,
-        }
-    }
+    'default': dj_database_url.config(
+        default='postgres://violett_user:violett_password@localhost:5432/violett_db',
+        conn_max_age=600,
+    )
 }
 
 
